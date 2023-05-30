ba8d8df5b44a5f235501885989b44ae4aef138f5
rebaderbs <rderbak@gmail.com>
2023-05-30 14:53:54 -0500

    changed anchor tags to NavLink

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,21 @@
 import { React } from "react";
+import { NavLink } from "react-bootstrap";
 
 function Header () {
     return (
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">
-                    <img src="books.png" alt="Mybrary Books" width="75" height="75" />
-                </a>
+                <NavLink class="navbar-brand" href="#">
+                    <img class="logo" src="books.png" alt="Mybrary Books" width="75" height="75" />
+                </NavLink>
 
                 <div class="nav justify-content-end" id="navbarToggleExternalContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Login</a>
+                            <NavLink class="nav-link" href="#">Login</NavLink>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Sign Up</a>
+                            <NavLink class="nav-link" href="#">Sign Up</NavLink>
                         </li>
                     </ul>
                 </div>
