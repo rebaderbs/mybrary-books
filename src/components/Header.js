adce9c1a26734cd603180d4b4fda443a1080f32c
rebaderbs <rderbak@gmail.com>
2023-05-30 14:18:36 -0500

    added images, header complete for now

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,24 @@ import { React } from "react";
 
 function Header () {
     return (
-        <div>This is the header</div>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img class="logo" src="books.png" alt="Mybrary Books" width="75" height="75" />
+                </a>
+
+                <div class="nav justify-content-end" id="navbarToggleExternalContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Login</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Sign Up</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     );
 }
 
