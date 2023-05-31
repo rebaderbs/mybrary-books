791b9a974b412e91bae7b4a58ddafb3a52d81661
rebaderbs <rderbak@gmail.com>
2023-05-31 13:22:17 -0500

    login and logout functionality are now working, dummy dashboard page is displaying

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,28 +3,38 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import Userfront from "@userfront/react";
 
 function Dashboard() {
+
+    const userData = JSON.stringify(Userfront.user, null, 2);
+    return (
+      <div>
+        <h2>Dashboard</h2>
+        <pre>{userData}</pre>
+        <button onClick={Userfront.logout}>Logout</button>
+      </div>
+    );
+
     function renderFn({ location }) {
         // If the user is not logged in, redirect to login
-        // if (!Userfront.accessToken()) {
-        //   return (
-        //     <Navigate
-        //       to={{
-        //         pathname: "/login",
-        //         state: { from: location },
-        //       }}
-        //     />
-        //   );
-        // }
+        if (!Userfront.accessToken()) {
+          return (
+            <Navigate
+              to={{
+                pathname: "/login",
+                state: { from: location },
+              }}
+            />
+          );
+        }
     
         // If the user is logged in, show the dashboard
-        const userData = JSON.stringify(Userfront.user, null, 2);
-        return (
-          <div>
-            <h2>Dashboard</h2>
-            <pre>{userData}</pre>
-            <button onClick={Userfront.logout}>Logout</button>
-          </div>
-        );
+        // const userData = JSON.stringify(Userfront.user, null, 2);
+        // return (
+        //   <div>
+        //     <h2>Dashboard</h2>
+        //     <pre>{userData}</pre>
+        //     <button onClick={Userfront.logout}>Logout</button>
+        //   </div>
+        // );
       }
     
       return <Routes><Route render={renderFn} /></Routes>;
