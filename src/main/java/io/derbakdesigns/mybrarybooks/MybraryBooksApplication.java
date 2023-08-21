fc1c1562a140868bef4a4e4367cdf356465d58a2
rebaderbs <rderbak@gmail.com>
2023-08-21 09:44:22 -0500

    initial files

---
+++ src/main/java/io/derbakdesigns/mybrarybooks/MybraryBooksApplication.java
@@ -0,0 +1,13 @@
+package io.derbakdesigns.mybrarybooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybraryBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybraryBooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/derbakdesigns/mybrarybooks/MybraryBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package io.derbakdesigns.mybrarybooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybraryBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
