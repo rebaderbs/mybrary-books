d8879bb23f305a82dbccbfff5c0b253f5044e9dd
rebaderbs <rderbak@gmail.com>
2023-08-28 11:00:19 -0500

    page header styling, footer updated

--- src/main/java/io/derbakdesigns/mybrarybooks/AuthenticationFilter.java
+++ src/main/java/io/derbakdesigns/mybrarybooks/AuthenticationFilter.java
@@ -21,7 +21,7 @@ public class AuthenticationFilter implements HandlerInterceptor {
     @Autowired
     AuthenticationController authenticationController;
 
-    private static final List<String> whitelist = Arrays.asList("/index", "/login", "/register", "/logout", "/css");
+//    private static final List<String> whitelist = Arrays.asList("/index", "/login", "/register", "/logout", "/css");
 
 //    private static boolean isWhitelisted(String path) {
 //        for (String pathRoot : whitelist) {
